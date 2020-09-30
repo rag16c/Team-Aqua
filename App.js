adb6d6ff454bfa93cc1e02aa3ed6e6a7bfba72c7
Riley Garrison <rileygarrison2022@u.northwestern.edu>
2020-09-30 17:25:08 -0400

    Basic popup
    Exit popup by touching remove modal text

--- App.js
+++ App.js
@@ -1,61 +1,86 @@
 import { StatusBar } from "expo-status-bar";
 import React, {Component} from "react";
-import { View, StyleSheet, Text, SafeAreaView, Dimensions, Image } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView, Dimensions, Image, Modal,TouchableHighlight} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { WebView } from "react-native-webview";
-import { Searchbar } from "react-native-paper";
+import { SearchBar } from "react-native-elements";
 
 
 export default function App() {
 
 	var text = '';
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [modalVisible, setModalVisible] = React.useState(true); 
 
   const onChangeSearch = query => {
 	  setSearchQuery(query);
-		
-  }
-  
-  const onSearchButtonPress = () => {
-	  setSearchQuery("I have searched");
-  }
-  return (
- 
-   <SafeAreaView style={styles.container}>
-	  <Searchbar
-        placeholder="Search"
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-		style={styles.searchBar}
-		onSubmitEditing={onSearchButtonPress}
-      />
-      <MapView
-        style={styles.mapContainer}
-        initialRegion={{
-          latitude: 42.047455,
-          longitude: -87.680657,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
-      >
-        <Marker coordinate={{ latitude: 42.035647, longitude: -87.669332 }}>
-          <Callout>
-            <CustomCalloutView />
-          </Callout>
-        </Marker>
-      </MapView>
-      <StatusBar style="auto" />
 
-	   <Text>
-	  {searchQuery}
+  }
+  const onSearchButtonPress = () => {
+    setSearchQuery("works!");
+  }
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <SearchBar
+          style={styles.searchBar}
+          platform="default"
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          onSubmitEditing={onSearchButtonPress}
+          lightTheme = {true}
+        />
+		<Callout>
+		<View>
+         <Modal
+           animationType="slide"
+           transparent={false}
+           visible={modalVisible} 
+	     >       
+           <Text> "Hello" </Text>
+	       <TouchableHighlight
+             style={styles.openButton}
+             onPress={() => {
+               setModalVisible(false);
+             }}
+           >
+	         <Text> "Hide Modal" </Text>
+	   
+	       </TouchableHighlight>
+         </Modal>
+        </View>
+		</Callout>
+        <MapView
+          style={styles.mapContainer}
+          initialRegion={{
+            latitude: 42.047455,
+            longitude: -87.680657,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+        >
+          <Marker coordinate={{ latitude: 42.035647, longitude: -87.669332}}>
+            <Callout>
+              <CustomCalloutView />
+            </Callout>
+          </Marker>
+      </MapView>
+      <StatusBar style="light-content" />
+	   <Text style={styles.results}>
+	    {searchQuery}
 	  </Text>
+
     </SafeAreaView>
+    </View>
   );
 }
 
 class CustomCalloutView extends React.Component {
     render() {
-        return (
+
+    return (
             <View>
                 <View>
                   <Text style={{
@@ -73,6 +98,26 @@ class CustomCalloutView extends React.Component {
     }
 }
 
+class ModalCallout extends React.Component {
+	
+render() {
+const [modalVisible, setModalVisible] = React.useState(false); 
+
+return (
+<View>
+<Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        
+      >
+</Modal>
+</View>
+
+)	
+}
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -84,11 +129,8 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height * 0.8,
   },
-  searchBar: {
-	flex: 0,
-	position: 'absolute',
-	top: 20,
-	left: 0,
-	right: 0,	
-  }
+  results: {
+    marginTop: '2%',
+  },
+
 });
